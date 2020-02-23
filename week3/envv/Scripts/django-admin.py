f446ddc428c0944ccd1b86d57b1d9f49e8dcd46d
islam <islamabdukarimov1999@mail.ru>
2020-02-23 22:47:56 +0600

    week3

---
+++ week3/envv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\islam\desktop\lessons\django\labs\week3\envv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
